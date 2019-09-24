3b61ae3b13c4dbd53b14d4255bf5344fc033e00c
Eric Bitikofer <EricBitikofer@Jeffs-Mac-Pro.local>
2019-09-24 17:50:11 -0400

    first day of training

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-app';
+
+  constructor () {
+    console.log(AppComponent.sval);
+    console.log(this.num_val);
+    this.storeNum();
+    console.log(this.addNumbers(7, 8));
+  }
+
+  static sval = 10;
+  num_val = 13;
+
+  childData: string;
+  userStatus: string;
+
+  storeNum(): void {
+    let local_num = 14;
+    console.log('local Num:' + local_num);
+  }
+
+  addNumbers(a, b): number {
+    return a + b;
+  }
+
+  catchChildEvent($event) {
+    this.childData = $event;
+  }
+
+  catchUserStatusEvent($event) {
+    this.userStatus = $event;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdComponent } from './prod/prod.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { FormsModule } from '@angular/forms';
+import { UserStatusComponent } from './user-status/user-status.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdComponent,
+    EmployeeComponent,
+    UserStatusComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
