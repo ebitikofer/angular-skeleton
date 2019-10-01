13aaabf00fbe4954ad14fd003bbd727c16755734
Eric Bitikofer <e.bitikofer@gmail.com>
2019-10-01 18:57:41 -0400

    Drag between multiple lists

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-notes',
@@ -12,6 +12,18 @@ export class NotesComponent implements OnInit {
 
   todo = ['clean room', 'wash car', 'practice drums', 'code', 'workout'];
 
+  done = [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep',
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
+
   currentTask = this.todo[0];
 
   ngOnInit() {
@@ -20,9 +32,16 @@ export class NotesComponent implements OnInit {
   updateCurrentTask(incomingTask) {
     this.currentTask = incomingTask;
   }
-
+  
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
   }
 
 }
