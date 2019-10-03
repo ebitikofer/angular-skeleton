3fd4f0b823669748ea79af45befe71cd17893aec
Eric Bitikofer <EricBitikofer@Jeffs-Mac-Pro.local>
2019-10-03 18:57:02 -0400

    Email auth changes

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,29 +1,41 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireAuth, AuthProviders } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseAuthService {
 
-  constructor(public afAuth: AngularFireAuth) {
-    
+  constructor (public afAuth: AngularFireAuth) {
+
   }
 
   // Returns true if user is logged in
 
   anonymousLogin() {
-    this.afAuth.auth.signInAnonymously().catch(function(error) {
+    this.afAuth.auth.signInAnonymously().catch(function (error) {
       // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-    
+      let errorCode = error.code;
+      let errorMessage = error.message;
+
       if (errorCode === 'auth/operation-not-allowed') {
         alert('You must enable Anonymous auth in the Firebase Console.');
       } else {
         console.error(error);
       }
     });
+  }
+
+  emailSignUp(email: string, password: string) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+      // .then(() => this.updateUserData())
+      .catch(error => console.log(error));
+  }
+
+  emailLogin(email: string, password: string) {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password)
+      //  .then(() => this.updateUserData())
+      .catch(error => console.log(error));
   }
 
   // firebase.auth().signInAnonymously().catch(function(error) {
