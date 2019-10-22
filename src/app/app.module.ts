52d05955e3be9fa728a6ee7c64ea407a2b9c538c
Eric Bitikofer <e.bitikofer@gmail.com>
2019-10-22 02:51:03 -0400

    Restructure app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Firebase
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { MatRadioModule } from '@angular/material/radio';
+
+// NGXS
 import { NgxsModule, Actions, Store } from '@ngxs/store';
 import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 
+// CDK UI
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
+// Material UI
+import { MatRadioModule } from '@angular/material/radio';
+
+// LOCAL
 
 // Envirment and API vars
 import { environment } from '../environments/environment';
 
-// Feature Modules
-import { ProductModule } from './molecules/product/product.module';
-import { OrderModule } from './molecules/order/order.module';
-import { LoginModule } from './molecules/login/login.module';
+// Routing
+import { AppRoutingModule } from './app-routing.module';
+
+// App
+import { AppComponent } from './app.component';
 
 // Template Components
 import { NavComponent } from './templates/nav/nav.component';
@@ -33,27 +42,31 @@ import { ContentComponent } from './templates/content/content.component';
 
 // Organism Components
 import { AuthenticationComponent } from './organisms/authentication/authentication.component';
+import { ChatComponent } from './organisms/chat/chat.component';
 
-// Molecule Components
-import { ProdComponent } from './molecules/prod/prod.component';
+// Molecule Components // Feature Modules
+import { ProductModule } from './molecules/product/product.module';
+import { OrderModule } from './molecules/order/order.module';
+import { LoginModule } from './molecules/login/login.module';
+import { SignupComponent } from './molecules/signup/signup.component';
+import { SidebarRoutesComponent } from './molecules/sidebar-routes/sidebar-routes.component';
+import { NotesComponent } from './molecules/notes/notes.component';
+import { E404Component } from './molecules/e404/e404.component';
+
 import { EmployeeComponent } from './molecules/employee/employee.component';
-import { UserStatusComponent } from './molecules/user-status/user-status.component';
 import { WidgetsComponent } from './molecules/widgets/widgets.component';
 import { ThemeSwitchComponent } from './molecules/theme-switch/theme-switch.component';
-import { NotesComponent } from './molecules/notes/notes.component';
-import { NumberIdentifierComponent } from './molecules/number-identifier/number-identifier.component';
-import { E404Component } from './molecules/e404/e404.component';
-import { SignupComponent } from './molecules/signup/signup.component';
+
+import { ProdComponent } from './molecules/prod/prod.component';
+import { UserStatusComponent } from './molecules/user-status/user-status.component';
 import { ProductAddComponent } from './molecules/product-add/product-add.component';
+import { NumberIdentifierComponent } from './molecules/number-identifier/number-identifier.component';
 
 // Atom Components
 import { HomeComponent } from './atoms/home/home.component';
 import { SelectListComponent } from './atoms/select-list/select-list.component';
 import { DateTimeComponent } from './atoms/date-time/date-time.component';
 import { ShrinkAnimationComponent } from './atoms/shrink-animation/shrink-animation.component';
-
-// Components
-import { AppComponent } from './app.component';
 
 // Directives
 import { ChangeTextDirective } from './core/directives/change-text.directive';
@@ -65,8 +78,6 @@ import { StringReversalPipe } from './core/pipes/string-reversal.pipe';
 
 // Services
 import { FirebaseAuthService } from './core/auth.service';
-import { SidebarRoutesComponent } from './molecules/sidebar-routes/sidebar-routes.component';
-import { ChatComponent } from './organisms/chat/chat.component';
 
 // State
 import { SettingsState } from './store/state/settings.state';
@@ -77,49 +88,52 @@ import { AuthState } from './store/state/auth.state';
 @NgModule({
   declarations: [
     AppComponent,
-    ProdComponent,
-    EmployeeComponent,
-    UserStatusComponent,
+
     NavComponent,
-    WidgetsComponent,
-    ChangeTextDirective,
-    ChangeColorDirective,
-    ThemeSwitchComponent,
-    NotesComponent,
-    NumberIdentifierComponent,
-    FloorPipe,
-    StringReversalPipe,
     SidebarComponent,
     ContentComponent,
-    E404Component,
-    SignupComponent,
-    ProductAddComponent,
-    HomeComponent,
+
     AuthenticationComponent,
+    ChatComponent,
+
+    SignupComponent,
+    SidebarRoutesComponent,
+    NotesComponent,
+    E404Component,
+
+    EmployeeComponent,
+    WidgetsComponent,
+    ThemeSwitchComponent,
+
+    ProdComponent,
+    UserStatusComponent,
+    ProductAddComponent,
+    NumberIdentifierComponent,
+
+    HomeComponent,
+    SelectListComponent,
     DateTimeComponent,
     ShrinkAnimationComponent,
-    SidebarRoutesComponent,
-    ChatComponent,
-    SelectListComponent
+
+    ChangeTextDirective,
+    ChangeColorDirective,
+    
+    FloorPipe,
+    StringReversalPipe,
   ],
   imports: [
-    BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    ProductModule,
-    OrderModule,
-    LoginModule,
-    AppRoutingModule,
-    HttpClientModule,
-    DragDropModule,
-    ScrollingModule,
-    BrowserAnimationsModule,
     CommonModule,
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireDatabaseModule,
-    MatRadioModule,
+
     NgxsModule.forRoot([UserState, AuthState, SettingsState], {
       developmentMode: !environment.production
     }),
@@ -135,6 +149,15 @@ import { AuthState } from './store/state/auth.state';
     // !environment.production ?
     NgxsLoggerPluginModule.forRoot({ disabled: environment.production }),
 
+    DragDropModule,
+    ScrollingModule,
+    MatRadioModule,
+
+    AppRoutingModule,
+
+    ProductModule,
+    OrderModule,
+    LoginModule,
   ],
   providers: [FirebaseAuthService],
   bootstrap: [AppComponent]
