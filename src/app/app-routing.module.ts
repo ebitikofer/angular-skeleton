597e82d326fe4a825e5d873e00a85c6ffd549c52
Eric Bitikofer <e.bitikofer@gmail.com>
2019-10-22 20:29:39 -0400

    Routing module restructure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { WidgetsComponent } from './atoms/widgets/widgets.component';
+
+import { ChatComponent } from './organisms/chat/chat.component';
+import { NotesComponent } from './organisms/notes/notes.component';
+
+import { ProductAddComponent } from './molecules/product-add/product-add.component';
+import { SignupComponent } from './molecules/signup/signup.component';
+import { E404Component } from './molecules/e404/e404.component';
+
 import { EmployeeComponent } from './molecules/employee/employee.component';
 import { ThemeSwitchComponent } from './molecules/theme-switch/theme-switch.component';
-import { NotesComponent } from './organisms/notes/notes.component';
-import { ChatComponent } from './organisms/chat/chat.component';
-import { E404Component } from './molecules/e404/e404.component';
-import { SignupComponent } from './molecules/signup/signup.component';
-import { ProductAddComponent } from './molecules/product-add/product-add.component';
+
 import { HomeComponent } from './atoms/home/home.component';
+import { WidgetsComponent } from './atoms/widgets/widgets.component';
 
 import { OrderSaveGuard } from './molecules/order/order-save.guard';
-
 import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
